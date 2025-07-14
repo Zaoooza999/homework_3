f352401af84be2fd9c00fdd3f4c6f54464f862c4
Max_Ust <maks_va@Inbox.ru>
2025-07-15 01:14:02 +0300

    add test

--- src/test/java/tests/TextBoxTests.java
+++ src/test/java/tests/TextBoxTests.java
@@ -33,24 +33,29 @@ public class TextBoxTests {
         $("#dateOfBirthInput").click();
         $(".react-datepicker__month-select").selectOption("January");
         $(".react-datepicker__year-select").selectOption("1995");
-        $(byText("28")).shouldBe(visible).click();
+        $(byText("28")).shouldBe(visible).click(); //насколько знаю про эту команду ничего не говорили, - нет ли способа лучше не через текст, и про shouldbe(visible) мы по моему не проходили, я сомневаюсь, что я сделал это правильно
         $("#subjectsInput").setValue("a");
         $(byText("Arts")).click();
         $(".custom-control.custom-checkbox.custom-control-inline [for=hobbies-checkbox-1]").click();
         $(".custom-control.custom-checkbox.custom-control-inline [for=hobbies-checkbox-2]").click();
         $("#uploadPicture").sendKeys("C:/Users/Max/Downloads/Telegram Desktop/image_2025-03-12_22-11-45.png");
         $("#currentAddress").setValue("California");
-        $(".col-md-4.col-sm-12").click();
+        $(".col-md-4.col-sm-12").click();//Не понимаю работу выпадающих списков, почти никогда не могу нажать на нижнеуровневый элемент почти всегда только верхний уровень
         $(byText("Haryana")).click();
         $("#city").click();
         $(byText("Panipat")).click();
         $("#submit").click();
-        Thread.sleep(50000);
-
-
-        $("#currentAddress").setValue("Street");
-        $("#submit").click();
-
-        $("[id=b_content]").shouldHave(text("https://selenideorg"));
+        $(".table-responsive").shouldHave(
+                text("Ivan Ivanov"),
+                text("Ivan@ivan.com"),
+                text("Male"),
+                text("1234567890"),
+                text("28 January,1995"),
+                text("Arts"),
+                text("Sports, Reading"),
+                text("image_2025-03-12_22-11-45.png"),
+                text("California"),
+                text("Haryana Panipat"),
+                text("Ivan Ivanov"));
     }
-}
+    }
